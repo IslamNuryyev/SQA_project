c42a4cf23b4b5ec1c3b4e62725ed7c6f5cbc8871
parekhbhargav25 <parekhbhargav25@gmail.com>
2023-03-30 14:50:12 -0400

    method tests finished

--- back_end/functions/test_bid.py
+++ back_end/functions/test_bid.py
@@ -7,28 +7,26 @@ from tempfile import NamedTemporaryFile
 def test_bid_statement_coverage():
     # Create temporary item file
     with NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-        temp_file.write("item1     seller1        100.00\n")
-        temp_file.write("item2     seller2        200.00\n")
-        temp_file.write("item3     seller3        50.00\n")
-        temp_file.write("item4     seller4        500.00\n")
-        temp_file.write("item5     seller5        300.00\n")
+        temp_file.write("item1               seller1         NONE           040 000100\n")
+        temp_file.write("item2               seller2         NONE           025 000200\n")
 
     # Test cases
-    bid("bid item1 seller1 bidder1 150.00\n", temp_file.name)
-    with open(temp_file.name, 'r') as f:
-        assert f.readlines()[0] == "item1     seller1        bidder1        100  150.0  \n"
+    bid("04 item1               seller1         bidder1          000150\n", temp_file.name)
 
-    bid("bid item2 seller2 bidder2 250.00\n", temp_file.name)
     with open(temp_file.name, 'r') as f:
-        assert f.readlines()[1] == "item2     seller2        bidder2        200  250.0  \n"
+        assert f.readlines()[0] == "item1               seller1         bidder1        040 150.0  \n"
 
-    bid("bid item5 seller5 bidder5 200.00\n", temp_file.name)
+    bid("04 item2               seller2         bidder2          000250\n", temp_file.name)
     with open(temp_file.name, 'r') as f:
-        assert f.readlines()[4] == "item5     seller5        bidder5        300  300.0  \n"
+        assert f.readlines()[1] == "item2               seller2         bidder2        025 250.0  \n"
 
-    bid("bid item6 seller6 bidder6 100.00\n", temp_file.name)
+    bid("04 item6               seller6         bidder6          000100\n", temp_file.name)
     with open(temp_file.name, 'r') as f:
         assert "item6" not in f.read()
+
+    bid("04 item6               seller7         bidder6          000100\n", temp_file.name)
+    with open(temp_file.name, 'r') as f:
+        assert "seller7" not in f.read()
 
     # Remove temporary item file
     os.unlink(temp_file.name)
